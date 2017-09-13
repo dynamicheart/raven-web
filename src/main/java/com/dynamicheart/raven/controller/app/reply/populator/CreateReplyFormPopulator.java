d5db2a14eafd1c4f261c2b3709efb995462dd82f
cbsh88 <cbsh88@github.com>
2017-09-13 14:00:44 +0800

    bug fix for admin house member count add post reply api

--- src/main/java/com/dynamicheart/raven/controller/app/reply/populator/CreateReplyFormPopulator.java
+++ src/main/java/com/dynamicheart/raven/controller/app/reply/populator/CreateReplyFormPopulator.java
@@ -1,4 +1,22 @@
 package com.dynamicheart.raven.controller.app.reply.populator;
 
-public class CreateReplyFormPopulator {
+import com.dynamicheart.raven.controller.app.reply.field.CreateReplyForm;
+import com.dynamicheart.raven.controller.common.AbstractDataPopulator;
+import com.dynamicheart.raven.model.reply.Reply;
+import com.dynamicheart.raven.utils.exception.ConversionException;
+
+public class CreateReplyFormPopulator extends AbstractDataPopulator<CreateReplyForm,Reply>{
+    @Override
+    protected Reply createTarget() {
+        return new Reply();
+    }
+
+    @Override
+    public Reply populate(CreateReplyForm createReplyForm, Reply reply) throws ConversionException {
+        reply.setContent(createReplyForm.getContent());
+        reply.setPolls(createReplyForm.getPolls());
+        reply.setRavenId(createReplyForm.getRavenId());
+        reply.setUserId(createReplyForm.getUserId());
+        return reply;
+    }
 }
